473074615b5ab3086fae4d33c0810fb22c38986c
Matt Joiner <anacrolix@gmail.com>
2022-12-12 10:57:00 +1100

    Add ConfigureOpenTelemetry

---
+++ honeycomb.go
@@ -0,0 +1,24 @@
+package app
+
+import (
+	"fmt"
+	"os"
+
+	_ "github.com/honeycombio/honeycomb-opentelemetry-go"
+	"github.com/honeycombio/opentelemetry-go-contrib/launcher"
+)
+
+func ConfigureOpenTelemetry() (cleanup func(), err error) {
+	cleanup = func() {}
+	otelShutdown, err := launcher.ConfigureOpenTelemetry(
+		launcher.WithResourceAttributes(map[string]string{
+			"fly.region":   os.Getenv("FLY_REGION"),
+			"fly.alloc_id": os.Getenv("FLY_ALLOC_ID"),
+		}))
+	if err != nil {
+		err = fmt.Errorf("setting up OTel SDK: %w", err)
+	} else {
+		cleanup = otelShutdown
+	}
+	return
+}
