4a373f593fdd85a27b1941f3c534f99800b0f07a
Matt Joiner <anacrolix@gmail.com>
2022-12-20 09:46:48 +1100

    Add fly.app_name

--- honeycomb.go
+++ honeycomb.go
@@ -14,6 +14,7 @@ func ConfigureOpenTelemetry() (cleanup func(), err error) {
 		launcher.WithResourceAttributes(map[string]string{
 			"fly.region":   os.Getenv("FLY_REGION"),
 			"fly.alloc_id": os.Getenv("FLY_ALLOC_ID"),
+			"fly.app_name": os.Getenv("FLY_APP_NAME"),
 		}))
 	if err != nil {
 		err = fmt.Errorf("setting up OTel SDK: %w", err)
