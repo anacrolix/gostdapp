b45ef8f5e83c005575226f52fc38142cb9533a32
Matt Joiner <anacrolix@gmail.com>
2023-08-29 20:35:18 +1000

    Break signalNotifyContextCause into its own file and update RunContext context cancellation error

--- app.go
+++ app.go
@@ -2,13 +2,11 @@ package app
 
 import (
 	"context"
-	"fmt"
-	"os"
-	"os/signal"
-
+	"errors"
 	"github.com/anacrolix/backtrace"
 	"github.com/anacrolix/envpprof"
 	"github.com/anacrolix/log"
+	"os"
 )
 
 // Deprecated: Use RunContext. Doesn't return on error.
@@ -22,7 +20,7 @@ func RunContext(
 	mainErr func(ctx context.Context) error,
 ) {
 	ctx, cancel := signalNotifyContextCause(context.Background(), os.Interrupt)
-	defer cancel(fmt.Errorf("RunOpts returned"))
+	defer cancel(errors.New("main returned"))
 	handleMainReturning(mainErr(ctx))
 }
 
@@ -32,60 +30,4 @@ func handleMainReturning(mainErr error) {
 		log.Printf("error in main: %v%s", mainErr, backtrace.Sprint(mainErr))
 		os.Exit(1)
 	}
-}
-
-// This is taken from the standard library, and modified to use context cancellation cause.
-func signalNotifyContextCause(parent context.Context, signals ...os.Signal) (ctx context.Context, stop context.CancelCauseFunc) {
-	ctx, cancel := context.WithCancelCause(parent)
-	c := &signalCtx{
-		Context: ctx,
-		cancel:  cancel,
-		signals: signals,
-	}
-	c.ch = make(chan os.Signal, 1)
-	signal.Notify(c.ch, c.signals...)
-	if ctx.Err() == nil {
-		go func() {
-			select {
-			case sig := <-c.ch:
-				c.cancel(fmt.Errorf("signal received: %v", sig))
-			case <-c.Done():
-			}
-		}()
-	}
-	return c, c.stop
-}
-
-type signalCtx struct {
-	context.Context
-
-	cancel  context.CancelCauseFunc
-	signals []os.Signal
-	ch      chan os.Signal
-}
-
-func (c *signalCtx) stop(cause error) {
-	c.cancel(cause)
-	signal.Stop(c.ch)
-}
-
-func (c *signalCtx) String() string {
-	var buf []byte
-	// We know that the type of c.Context is context.cancelCtx, and we know that the
-	// String method of cancelCtx returns a string that ends with ".WithCancel".
-	name := c.Context.(fmt.Stringer).String()
-	name = name[:len(name)-len(".WithCancel")]
-	buf = append(buf, "signal.NotifyContext("+name...)
-	if len(c.signals) != 0 {
-		buf = append(buf, ", ["...)
-		for i, s := range c.signals {
-			buf = append(buf, s.String()...)
-			if i != len(c.signals)-1 {
-				buf = append(buf, ' ')
-			}
-		}
-		buf = append(buf, ']')
-	}
-	buf = append(buf, ')')
-	return string(buf)
 }
---
+++ signal-notify-context-cause.go
@@ -0,0 +1,64 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+)
+
+// This is taken from the standard library, and modified to use context cancellation cause.
+func signalNotifyContextCause(parent context.Context, signals ...os.Signal) (ctx context.Context, stop context.CancelCauseFunc) {
+	ctx, cancel := context.WithCancelCause(parent)
+	c := &signalCtx{
+		Context: ctx,
+		cancel:  cancel,
+		signals: signals,
+	}
+	c.ch = make(chan os.Signal, 1)
+	signal.Notify(c.ch, c.signals...)
+	if ctx.Err() == nil {
+		go func() {
+			select {
+			case sig := <-c.ch:
+				c.cancel(fmt.Errorf("signal received: %v", sig))
+			case <-c.Done():
+			}
+		}()
+	}
+	return c, c.stop
+}
+
+type signalCtx struct {
+	context.Context
+
+	cancel  context.CancelCauseFunc
+	signals []os.Signal
+	ch      chan os.Signal
+}
+
+func (c *signalCtx) stop(cause error) {
+	c.cancel(cause)
+	signal.Stop(c.ch)
+}
+
+func (c *signalCtx) String() string {
+	var buf []byte
+	// We know that the type of c.Context is context.cancelCtx, and we know that the
+	// String method of cancelCtx returns a string that ends with ".WithCancel".
+	name := c.Context.(fmt.Stringer).String()
+	name = name[:len(name)-len(".WithCancel")]
+	buf = append(buf, "signal.NotifyContext("+name...)
+	if len(c.signals) != 0 {
+		buf = append(buf, ", ["...)
+		for i, s := range c.signals {
+			buf = append(buf, s.String()...)
+			if i != len(c.signals)-1 {
+				buf = append(buf, ' ')
+			}
+		}
+		buf = append(buf, ']')
+	}
+	buf = append(buf, ')')
+	return string(buf)
+}
